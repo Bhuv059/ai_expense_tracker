a00e278b31c5163b8027739f8462203459b3bfc3
Bhuv <bhuv059@gmail.com>
2025-08-16 23:02:00 +0530

    Navbar, guest, about and contact pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import {currentUser} from "@clerk/nextjs/server";
+import Guest from "@/(components)/Guest";
+
+
+export default async function Home() {
+    const user = await currentUser()
+
+    if(!user) return <Guest />
+
   return (
     <main>
       <p>Home page</p>
