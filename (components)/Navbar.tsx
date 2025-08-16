6728d0dcf41a2707675e12ada4494b32d861ece0
Bhuv <bhuv059@gmail.com>
2025-08-16 16:12:32 +0530

    Clerk Authentication

---
+++ (components)/Navbar.tsx
@@ -0,0 +1,32 @@
+import {checkUser} from '@/lib/checkUser'
+const Navbar = () => {
+	const user = checkUser();
+	console.log(user);
+	return (
+		<div>Navbar</div>
+	)
+}
+export default Navbar
+
+/*
+import {SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs";
+const Navbar = () => {
+	return (
+
+	<header className="flex justify-end items-center p-4 gap-4 h-16">
+		<SignedOut>
+			<SignInButton />
+			<SignUpButton>
+				<button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+					Sign Up
+				</button>
+			</SignUpButton>
+		</SignedOut>
+		<SignedIn>
+			<UserButton />
+		</SignedIn>
+	</header>
+	)
+}
+export default Navbar
+*/
